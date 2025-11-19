6d4c39cba4c8f4d618305e3421ed2d0c1473f780
ScottK567 <kirkpatrickm@berea.edu>
2025-11-19 13:24:27 -0500

    Have the inital screen in a class format setup and ready to use. Added necessary comments and deleted some uneccessary parts.

--- Milestone2.py
+++ Milestone2.py
@@ -8,72 +8,22 @@
 #             Subtask I.A.3: Display them on screen.*
 #             Subtask I.A.4: Use turtle.screen to create a screen.*
 #             Subtask I.A.5: Set screen width and height.*
-#
-#
-# '''
-#
-#
-#
-# ## Import all modules necessary for the game
-#
-# import turtle
-# import random
-# import pygame
-#
-# class Game_Setup:
-#
-#     def __init__(self):
-#         self.screen_width = 800
-#         self.screen_height = 600
-#         self.screen = pygame.display.set_mode((self.screen_width,self.screen_height))  # Requesting pygame to display and save it under the veriable Screen.
-#     # Fill screen
-#         self.screen.fill((255, 255, 255))
-#         self.run = True  # Variable to keep screen running
-#
-#
-#     # Draw the player image
-#         #self.screen.blit(npc, (50, 50))  # set the player size to
-#
-#     def run(self):
-#
-#
-#         #pygame.display.set_caption("pygames_character.png")  # Crate pycham npc
-#
-#         while self.run:  # This loop keeps the screen running until user click X button.
-#             for event in pygame.event.get():
-#                 if event.type == pygame.QUIT:
-#                     run = False
-#
-#             pygame.display.update()
-#             pygame.quit()  # Use to exit the screen
-#         #npc = pygame.image.load("pygames_character.png")  # make sure file exists
-#
-# # Setting the screen size of the game
-#
-#
-#
-#
-# def main():
-#
-#     pygame.init()  # pygame initializer
-#
-#     game = Game_Setup()
-#     game.run()
-#
-# main()
-#
-#
 
 import pygame
 
 class Game:
     def __init__(self):
         pygame.init()
+        #Initializes the game
         self.screen = pygame.display.set_mode((800, 600))
+        #Sets the screen size for the game
         self.screen.fill((255,255,255))
+        #Sets the screen color for the game(white for now probably change in the future)
         self.run = True
+        #Makes the game currently set to run
 
     def game_time(self):
+        #Main loop for the game to run essentially saying while the game is running doe everything within this.
         while self.run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
